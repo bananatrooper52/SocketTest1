3f05d1c465f64168f54fc4fbbb2535a5c69c54fc
bananatrooper52 <elijahriver11123@Elijahs-MacBook-Air.local>
2016-08-25 20:05:47 -1000

    finished client class

--- src/main/Client.java
+++ src/main/Client.java
@@ -5,37 +5,65 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
-public static void main(String[] args) {
-		
-	int port = 0;
-	
+	static int port = 0;
+	static boolean running = true;
+	public static Socket socket;
+	public static PrintWriter out;
+	public static BufferedReader in;
+	public static BufferedReader sysIn;
+
+	public static void main(String[] args) {
+
 		try {
-			
-			BufferedReader sysIn = new BufferedReader(new InputStreamReader(System.in));
+
+			sysIn = new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("Host IP: ");
 			String hostIP = sysIn.readLine();
-			port = Integer.parseInt(sysIn.readLine());
-			Socket socket = new Socket(hostIP, port);
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			
-			String userInput;
-			while ((userInput = sysIn.readLine()) != null) {
-			    out.println(userInput);
-			    System.out.println("echo: " + in.readLine());
+			if (hostIP == "" || hostIP == null) {
+				hostIP = "192.168.0.16";
 			}
-			
-			socket.close();
-			out.close();
-			in.close();
-			sysIn.close();
-			
+			System.out.print("Port: ");
+			port = Integer.parseInt(sysIn.readLine());
+			socket = new Socket(hostIP, port);
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			String inputLine = "";
+
+			while (running) {
+
+				// SEND
+				if (sysIn.ready()) {
+					inputLine = sysIn.readLine();
+					out.println(inputLine);
+				}
+
+				// RECEIVE
+				if (in.ready())
+					System.out.println(in.readLine());
+
+				// DO COMMANDS
+				if (inputLine.toLowerCase() == "/exit") {
+					quit();
+				}
+			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
+	public static void quit() throws IOException {
+		running = false;
+		socket.close();
+		out.close();
+		in.close();
+		sysIn.close();
+	}
+
 }
