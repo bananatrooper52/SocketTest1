b933bf899bf1866ee3e1c5f05de5e46eaf7d7863
bananatrooper52 <bananatrooper52@gmail.com>
2016-08-25 20:05:21 -1000

    finished server class

--- src/main/Server.java
+++ src/main/Server.java
@@ -9,35 +9,68 @@ import java.net.Socket;
 
 public class Server {
 
+	static ServerSocket serverSocket;
+	static Socket clientSocket;
+	static PrintWriter out;
+	static BufferedReader in, sysIn;
+	static boolean running = true;
+
 	public static void main(String args[]) {
 
 		int port = 0;
-		
+
 		try {
 
-			BufferedReader sysIn = new BufferedReader(new InputStreamReader(System.in));
+			sysIn = new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("Port: ");
 			port = Integer.parseInt(sysIn.readLine());
-			ServerSocket serverSocket = new ServerSocket(port);
-			Socket clientSocket = serverSocket.accept();
-			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			
-			String inputLine;
-			while((inputLine = in.readLine()) != null) {
-				out.println(inputLine);
+			serverSocket = new ServerSocket(port);
+			System.out.println("Connected on port " + port + ", waiting for client to join...");
+			clientSocket = serverSocket.accept();
+			System.out.println("Client joined");
+			out = new PrintWriter(clientSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+			String inputLine = "";
+
+			while (running) {
+
+				// SEND
+				
+				if(sysIn.ready()) {
+					inputLine = sysIn.readLine();
+					out.println(inputLine);
+				}
+
+				// RECEIVE
+				if (in.ready())
+					System.out.println(in.readLine());
+
+				// DO COMMANDS
+				if (inputLine.toLowerCase() == "/exit") {
+					quit();
+				}
+				
 			}
-			
-			serverSocket.close();
-			clientSocket.close();
-			in.close();
-			sysIn.close();
-			out.close();
 
 		} catch (IOException e) {
 			System.err.println("Exception on port: " + port);
 			e.printStackTrace();
 		}
 
+	}
+
+	public static void quit() throws IOException {
+
+		running = false;
+
+		serverSocket.close();
+		clientSocket.close();
+		in.close();
+		sysIn.close();
+		out.close();
+
+		System.exit(0);
 	}
 
 }
